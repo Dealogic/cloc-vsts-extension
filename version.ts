53a01dbb4ebae638e71f3060ac35b69426f374c8
József Kanczler <jozsef.kanczler@gmail.com>
2017-08-14 12:41:12 +0200

    version 0.1.0 and icon for task

--- version.ts
+++ version.ts
@@ -1,1 +1,1 @@
-export default "__GitVersion.SemVer__".replace("GitVersion.SemVer", "1.0.0").replace(/__/g, "");
+export default "__GitVersion.SemVer__".replace("GitVersion.SemVer", "0.1.0").replace(/__/g, "");
